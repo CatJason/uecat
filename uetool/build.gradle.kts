84ff870f52db54da1bdba32772e6b3d041d2564e
wangzhijun <zhijun.wang@maxjia.com>
2023-12-25 23:10:28 +0800

    设置依赖

--- uetool/build.gradle.kts
+++ uetool/build.gradle.kts
@@ -56,5 +56,10 @@ android {
 }
 
 dependencies {
+    implementation(project(":scalpellibrary"))
+    implementation("com.facebook.fresco:fresco:3.1.0")
+    implementation("com.github.tiann:FreeReflection:3.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
 }
