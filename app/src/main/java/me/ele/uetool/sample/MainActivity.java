211def99170e6e97fa9b746808e232f78ea179aa
wangzhijun <zhijun.wang@maxjia.com>
2023-12-26 10:39:50 +0800

    UECat

--- app/src/main/java/me/ele/uetool/sample/MainActivity.java
+++ app/src/main/java/me/ele/uetool/sample/MainActivity.java
@@ -18,6 +18,7 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
+import me.ele.uetool.UETool;
 import me.ele.uetool.sample.ui.recyclerviewsample.RecyclerViewActivity;
 
 public class MainActivity extends AppCompatActivity {
